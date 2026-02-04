bedfd5767298f8b46bf4ec8f829a9e636ce74873
Isaac Scott <isaac.scott@transportforthenorth.com>
2026-02-04 14:14:16 +0000

    Update triprate_factors.py

--- src/caf/mat/prior_adjustment/triprate_factors.py
+++ src/caf/mat/prior_adjustment/triprate_factors.py
@@ -106,8 +106,9 @@ def tripend_factors(synthetic,
     prior_te['O'] = (synthetic_dvecs['O'].filter_segment_value('direction_od', 1) / hb_prod_uc)
     prior_te['D'] = (synthetic_dvecs['D'].filter_segment_value('direction_od', 1) / hb_attr_uc)
 
-    for orig in ['P','A']:
-        (prior_te[orig] * post_prior[orig]).save(out_dir / f"tr_factors_{orig}.dvec")
+    for orig in ['O','D']:
+        factor = prior_te[orig] * post_prior[orig]
+        factor.add_segments(['p']).remove_segment('userclass').save(out_dir / f"post_me_adj_{orig}.dvec")
 
 
 def mts_adj_factors(postme_dir: Path,
